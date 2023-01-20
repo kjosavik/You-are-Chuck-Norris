46fca818f42d34c9f2b6782db983883f82869377
Erik Kjosavik <erik.kjosavik@gmail.com>
2023-01-20 22:03:12 +0000

    Add support for Ninafina

--- script.js
+++ script.js
@@ -1,40 +1,57 @@
 function getName() {
   const url = document.URL;
-  const indexOfPeriod = url.indexOf('.');
+  const indexOfPeriod = url.indexOf(".");
   let name = url.substring(8, indexOfPeriod);
   return firstLetterToUpperCase(name);
 }
 
 async function fetchJoke() {
-  const url = 'https://api.chucknorris.io/jokes/random';
-  const response = await fetch(url)
+  const url = "https://api.chucknorris.io/jokes/random";
+  const response = await fetch(url);
   const json = await response.json();
   return json.value;
 }
 
 function getJokeWithNewName(name, joke) {
-  const chuck = 'Chuck Norris';
-  let index = joke.indexOf(chuck);
+  const chuck = "Chuck Norris";
+  joke = replaceAllOccurances(joke, chuck, name);
+  joke = replaceAllOccurances(joke, `${name}'`, `${name}'s`);
+  return joke;
+}
+
+function getJokeWithNewGender(joke) {
+  joke = replaceAllOccurances(joke, "he", "she");
+  joke = replaceAllOccurances(joke, "him", "her");
+  joke = replaceAllOccurances(joke, "his", "her");
+  return joke;
+}
+
+function replaceAllOccurances(joke, toBeReplaced, replaceWith) {
+  const regex = new RegExp("\\b" + toBeReplaced + "\\b");
+  let index = joke.search(regex);
+  console.log(joke);
   while (index >= 0) {
-    joke = joke.replace(chuck, name);
-    joke = joke.replace(`${name}'`, `${name}'s`);
-    index = joke.indexOf(chuck);
+    joke = joke.replace(regex, replaceWith);
+    index = joke.search(regex);
   }
   return joke;
 }
 
 async function setJoke(name) {
   const joke = await fetchJoke();
-  const jokeWithName = getJokeWithNewName(name, joke);
-  const element = document.getElementById('joke');
-  element.innerText = firstLetterToUpperCase(jokeWithName);
+  let fixedJoke = getJokeWithNewName(name, joke);
+  if (name === "Ninafina") {
+    fixedJoke = getJokeWithNewGender(fixedJoke);
+  }
+  const element = document.getElementById("joke");
+  element.innerText = firstLetterToUpperCase(fixedJoke);
 }
 
 async function setButton(name) {
-  const button = document.getElementById('button');
+  const button = document.getElementById("button");
   console.log(button);
-  button.addEventListener('click', () => {
-    setJoke(name)
+  button.addEventListener("click", () => {
+    setJoke(name);
   });
 }
 
@@ -46,5 +63,5 @@ const name = getName();
 document.title = `Ka faen ${name}?`;
 setJoke(name);
 window.onload = () => {
-  setButton(name)
+  setButton(name);
 };
